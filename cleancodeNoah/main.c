e526e026015ec747901c69b68ec57919c455a1f7
Noah8820 <noah.deboni@noseryoung.com>
2022-10-05 15:33:37 +0200

    uploading not clean code

---
+++ cleancodeNoah/main.c
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: noahd
+ *
+ * Created on 5. Oktober 2022, 14:38
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    int a = 5;
+    int b = 10;
+    int sum;
+    int sum1;
+    int du;
+
+    printf("Dies ist kein clean code. \nDies ist kein clean code. \nDies ist kein clean code. \nDies ist kein clean code. \nDies ist kein clean code. \n");
+
+    sum = a + b;
+
+    scanf("&d", &sum1);
+
+    printf("%d", sum);
+
+
+    return (EXIT_SUCCESS);
+}
+
